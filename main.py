3b278daa4da7f23666458209d3fa71a1555f27a4
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2023-05-03 13:58:00 +1000

    Completed text and csv files lesson

---
+++ main.py
@@ -0,0 +1,33 @@
+# with open('shopping-list.txt', 'r') as f:
+#   for line in f:
+#     print(f'Item: {line.strip()}')
+#   # data = f.readline().strip()
+#   # print(data)
+
+# shows = [
+#   'The Mandalorian',
+#   'The Witcher',
+#   'The X Files',
+#   'Star Trek: Picard'
+# ]
+
+# with open('tv-shows.txt', 'w') as f:
+#   f.write('\n'.join(shows))
+import csv
+
+# with open('people.csv') as f:
+#   reader = csv.DictReader(f) # Create a reader object based on the file object
+#   for row in reader:
+#     # print(row)
+#     print(f"{row['name']} is {row['age']} years old and {row['height']}cm tall")
+
+menu = [
+  {'item': 'Cappuccino', 'price': 5.50},
+  {'item': 'English Breakfast Tea', 'price': 5},
+  {'item': 'Blueberry Muffin', 'price': 6}
+]
+
+with open('cafe-menu.csv', 'w') as f:
+  writer = csv.DictWriter(f, menu[0].keys())
+  writer.writeheader()
+  writer.writerows(menu)
